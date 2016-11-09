83b7d89e3044227977a5ef7bca6b71620fe7c45c
goldGreatChen <chenjinwei1109@gmail.com>
2016-11-09 22:16:58 +0800

    插入排序

--- src/main/java/com/cn/chen/util/SortedUtils.java
+++ src/main/java/com/cn/chen/util/SortedUtils.java
@@ -6,6 +6,29 @@ import java.util.Arrays;
  * Created by Administrator on 2016/11/9.
  */
 public class SortedUtils {
+
+    /**
+     * 插入排序
+     * @param arr
+     * @return
+     */
+    public static  Comparable[] insertSort(Comparable[] arr){
+        for (int i = 1; i < arr.length; i++) {
+            for (int j = i-1; j >=0 ; j--) {
+                if((j>0&&less(arr[i],arr[j])&&less(arr[j-1],arr[i]))||(j==0&&less(arr[i],arr[j]))) {
+                    Comparable temp = arr[i];
+                    for (int k = i; k > j; k--) {
+                        arr[k] = arr[k - 1];
+                    }
+                    arr[j] = temp;
+                }
+            }
+        }
+        return arr;
+    }
+
+
+
     /**
      * 选择排序  其本质是将未排序数组中的最小的和围排序数组中的第一个数交换位置
      * @param arr
@@ -42,12 +65,22 @@ public class SortedUtils {
         return j;
     }
 
+    /**
+     * a<=b返回true 反之返回false
+     * @param a
+     * @param b
+     * @return
+     */
+    public static boolean less(Comparable a,Comparable b){
+        return a.compareTo(b)<=0;
+    }
+
     public static void main(String[] args) {
-        int[] arr=new int[10];
-        for (int i = 0; i < 10; i++) {
+        Integer[] arr=new Integer[100];
+        for (int i = 0; i < 100; i++) {
             arr[i]= (int) (Math.random()*100);
         }
         System.out.println(Arrays.toString(arr));
-        System.out.println(Arrays.toString(sectionSort(arr)));
+        System.out.println(Arrays.toString(insertSort(arr)));
     }
 }
